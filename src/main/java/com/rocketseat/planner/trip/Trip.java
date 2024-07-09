87c57bccc17ec5a8c71ebcb5df075db59498da93
José da Silva <josesilva23p@gmail.com>
2024-07-09 17:17:25 +0100

    create trip entity and repository

---
+++ src/main/java/com/rocketseat/planner/trip/Trip.java
@@ -0,0 +1,37 @@
+package com.rocketseat.planner.trip;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Trip {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @Column(nullable = false)
+  private String destination;
+
+  @Column(name = "starts_at", nullable = false)
+  private LocalDateTime startsAt;
+
+  @Column(name = "ends_at", nullable = false)
+  private LocalDateTime endsAt;
+
+  @Column(name = "is_confirmed", nullable = false)
+  private Boolean isConfirmed;
+
+  @Column(name = "owner_name", nullable = false)
+  private String ownerName;
+
+  @Column(name = "owner_email", nullable = false)
+  private String ownerEmail;
+
+}
